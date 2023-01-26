8545e918ec82e6826524b47bc9663418e8c290e8
Milton Navarro <milton.j.navarro.p@gmail.com>
2023-01-26 16:22:48 -0600

    pointing to demo api in the cloud

--- demo/utils/Endpoints.js
+++ demo/utils/Endpoints.js
@@ -1,4 +1,6 @@
-export const ROOT = 'http://192.168.1.247:45455/api/';
+// export const ROOT = 'http://192.168.1.247:45455/api/';
 // export const ROOT = 'https://navarrow_crud-employee/api/';
+export const ROOT = 'https://crud-employee.navarroweb.net/api/';
 
 export const EMPLOYEE = ROOT + 'Employee/';
+export const GET_BY_ID = 'getById/';
